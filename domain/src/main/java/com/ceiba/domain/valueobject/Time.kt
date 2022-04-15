aa7114eef6812bef39183ef538002b9be822e8da
Juan Sebastian Grisales Muñoz <sebastian.grisales@ceiba.com.co>
2022-04-15 15:17:12 -0500

    Se agrega constructor para crear el tiempo en el que el vehículo ha estado en el parqueadero

--- domain/src/main/java/com/ceiba/domain/valueobject/Time.kt
+++ domain/src/main/java/com/ceiba/domain/valueobject/Time.kt
@@ -1,10 +1,16 @@
 package com.ceiba.domain.valueobject
 
-class Time {
+class Time(startDateTime: String, endDateTime: String, day: String) {
     var startDateTime: String? = null
     var endDateTime: String? = null
     var day: String? = null
     var numberDays: Int? = null
     var numberHours: Int? = null
+
+    init {
+        this.startDateTime = startDateTime
+        this.endDateTime = endDateTime
+        this.day = day
+    }
 
 }
