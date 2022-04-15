a6f8c8854b2172be817573d796f2340667d14217
Juan Sebastian Grisales Muñoz <sebastian.grisales@ceiba.com.co>
2022-04-15 15:58:25 -0500

    Se agrega implementación de ParkingRepository en el ParkingService para realizar el ingreso de un vehículo

--- domain/src/main/java/com/ceiba/domain/service/ParkingService.kt
+++ domain/src/main/java/com/ceiba/domain/service/ParkingService.kt
@@ -1,4 +1,16 @@
 package com.ceiba.domain.service
 
-class ParkingService {
+import com.ceiba.domain.aggregate.Parking
+import com.ceiba.domain.repository.ParkingRepository
+
+class ParkingService(parkingRepository: ParkingRepository) {
+    private var parkingRepository: ParkingRepository? = null
+
+    init {
+        this.parkingRepository = parkingRepository
+    }
+
+    fun enterVehicle(parking: Parking) {
+        parkingRepository?.enterVehicle(parking)
+    }
 }
