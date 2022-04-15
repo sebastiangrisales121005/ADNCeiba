a75fbbc8489e99ce79710ffc05fa126614db6e29
Juan Sebastian Grisales Muñoz <sebastian.grisales@ceiba.com.co>
2022-04-15 16:36:04 -0500

    Se agrega inyección de dependecia a la implemetación de ParkingServiceApplication y se agrega implementación de dagger en el build.gradle de  application

--- application/src/main/java/com/ceiba/application/service/ParkingServiceApplication.kt
+++ application/src/main/java/com/ceiba/application/service/ParkingServiceApplication.kt
@@ -2,8 +2,11 @@ package com.ceiba.application.service
 
 import com.ceiba.domain.aggregate.Parking
 import com.ceiba.domain.service.ParkingService
+import javax.inject.Inject
 
-class ParkingServiceApplication {
+class ParkingServiceApplication @Inject constructor() {
+
+    @Inject
     var parkingService: ParkingService? = null
 
     fun enterVehicle(parking: Parking) {
