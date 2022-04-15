6669f1e0621b899e2875d393f821f01953e39bc2
Juan Sebastian Grisales Muñoz <sebastian.grisales@ceiba.com.co>
2022-04-15 16:16:32 -0500

    Se agrega implementación de parkingService en la clase de ParkingServiceApplication con el método de enterVehicle

--- application/src/main/java/com/ceiba/application/service/ParkingServiceApplication.kt
+++ application/src/main/java/com/ceiba/application/service/ParkingServiceApplication.kt
@@ -1,8 +1,12 @@
 package com.ceiba.application.service
 
+import com.ceiba.domain.aggregate.Parking
 import com.ceiba.domain.service.ParkingService
 
 class ParkingServiceApplication {
+    var parkingService: ParkingService? = null
 
-
+    fun enterVehicle(parking: Parking) {
+        parkingService?.enterVehicle(parking)
+    }
 }
