02c759f10e1409327ae56e08f5e91e78bdbf63be
Juan Sebastian Grisales Muñoz <sebastian.grisales@ceiba.com.co>
2022-04-15 15:12:19 -0500

    Se agrega constructor que permite crear un vehículo con validación para que asigne el cilindraje en caso de que sólo sea una moto

--- domain/src/main/java/com/ceiba/domain/entity/Vehicle.kt
+++ domain/src/main/java/com/ceiba/domain/entity/Vehicle.kt
@@ -1,9 +1,19 @@
 package com.ceiba.domain.entity
 
-class Vehicle {
+class Vehicle(licensePlate: String, vehicleType: String?, cylinderCapacity: Int) {
     var licensePlate: String? = null
     var vehicleType: String? = null
     var cylinderCapacity: Int? = null
+
+    init {
+        this.licensePlate = licensePlate
+        this.vehicleType = vehicleType
+        if (this.vehicleType.equals("MOTO")) {
+            this.cylinderCapacity = cylinderCapacity
+        } else {
+            //Lanzar mensaje o excepción que indique que al ser un carro no es necesario obtener el cilindraje
+        }
+    }
 
 
 }
