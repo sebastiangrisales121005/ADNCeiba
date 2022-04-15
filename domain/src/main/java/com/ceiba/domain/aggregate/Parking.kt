1dd902a84d78097199b31566a10412607e8d4dc3
Juan Sebastian Grisales Muñoz <sebastian.grisales@ceiba.com.co>
2022-04-15 15:30:26 -0500

    Se agrega constructor en la clase Parking para ingresar los vehículos al parqueadero

--- domain/src/main/java/com/ceiba/domain/aggregate/Parking.kt
+++ domain/src/main/java/com/ceiba/domain/aggregate/Parking.kt
@@ -4,10 +4,16 @@ import com.ceiba.domain.entity.Vehicle
 import com.ceiba.domain.valueobject.Payment
 import com.ceiba.domain.valueobject.Time
 
-class Parking {
+class Parking(vehicle: Vehicle, time: Time, payment: Payment) {
     var numberVehicles: Int? = null
     var vehicle: Vehicle? = null
     var time: Time? = null
     var payment: Payment? = null
+
+    init {
+        this.vehicle = vehicle
+        this.time = time
+        this.payment = payment
+    }
 
 }
