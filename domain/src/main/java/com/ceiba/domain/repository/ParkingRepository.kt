33ce2fa2760af21b36f6ea11834e6e11d1090665
Juan Sebastian Grisales Muñoz <sebastian.grisales@ceiba.com.co>
2022-04-15 15:39:14 -0500

    Se agrega paquete repository con la interface para ejecutar las acciones del parqueadero y se crea el paquete service con la clase ParkingService

---
+++ domain/src/main/java/com/ceiba/domain/repository/ParkingRepository.kt
@@ -0,0 +1,7 @@
+package com.ceiba.domain.repository
+
+import com.ceiba.domain.aggregate.Parking
+
+interface ParkingRepository {
+    fun enterVehicle(parking: Parking)
+}
---
+++ domain/src/main/java/com/ceiba/domain/service/ParkingService.kt
@@ -0,0 +1,4 @@
+package com.ceiba.domain.service
+
+class ParkingService {
+}
