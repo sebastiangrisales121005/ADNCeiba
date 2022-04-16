14f6d52f767eaeaecc7c2f586adbd8663e3cd017
Juan Sebastian Grisales Muñoz <sebastian.grisales@ceiba.com.co>
2022-04-16 15:21:11 -0500

    Se agrega clase de ParkingRepositoryRoom y se implementa la interface de ParkingRepository, además se agrega la implementación del módulo de domain en el build.gradle de dataAccess

---
+++ insfraestructure/dataAccess/src/main/java/com/ceiba/dataaccess/repository/ParkingRepositoryRoom.kt
@@ -0,0 +1,10 @@
+package com.ceiba.dataaccess.repository
+
+import com.ceiba.domain.aggregate.Parking
+import com.ceiba.domain.repository.ParkingRepository
+
+class ParkingRepositoryRoom: ParkingRepository {
+    override fun enterVehicle(parking: Parking) {
+        
+    }
+}
