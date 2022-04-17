6e963d7205149aac7f38bf788e1bca1003960e32
Juan Sebastian Grisales Muñoz <sebastian.grisales@ceiba.com.co>
2022-04-17 13:08:28 -0500

    Se agrega atributos necesarios para hacer uso del ParkingDto y se agregan las respectivas anotaciones de Room

--- insfraestructure/dataAccess/src/main/java/com/ceiba/dataaccess/dto/ParkingDto.kt
+++ insfraestructure/dataAccess/src/main/java/com/ceiba/dataaccess/dto/ParkingDto.kt
@@ -1,4 +1,14 @@
 package com.ceiba.dataaccess.dto
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
 class ParkingDto  {
+    @PrimaryKey var licensePlate: String? = null
+    var vehicleType: String? = null
+    var cylinderCapacity: Int? = null
+    var startDateTime: String? = null
+    var endDateTime: String? = null
+    var day: String? = null
 }
