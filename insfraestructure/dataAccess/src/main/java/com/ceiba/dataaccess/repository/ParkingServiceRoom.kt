b3477ef117ac9582a3d70f400817598bbde41c68
Juan Sebastian Grisales Muñoz <sebastian.grisales@ceiba.com.co>
2022-04-17 13:22:52 -0500

    Se agrega anotación de inserta a la función respectiva

--- insfraestructure/dataAccess/src/main/java/com/ceiba/dataaccess/repository/ParkingServiceRoom.kt
+++ insfraestructure/dataAccess/src/main/java/com/ceiba/dataaccess/repository/ParkingServiceRoom.kt
@@ -1,9 +1,11 @@
 package com.ceiba.dataaccess.repository
 
 import androidx.room.Dao
+import androidx.room.Insert
 import com.ceiba.dataaccess.dto.ParkingDto
 
 @Dao
 interface ParkingServiceRoom {
+    @Insert
     suspend fun insertVehicle(parkingDto: ParkingDto)
 }
