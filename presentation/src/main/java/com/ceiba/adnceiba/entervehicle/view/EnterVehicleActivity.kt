b8c72afa5d20e84d839c881cca7766f8934041cd
Juan Sebastian Grisales Muñoz <sebastian.grisales@ceiba.com.co>
2022-04-18 11:08:57 -0500

    Se agrega spinner para ingresar el tipo de vehiculo para el parqueadero y se agrega sus propiedades correspondientes junto con el método que obtiene la opción seleccionada

--- presentation/src/main/java/com/ceiba/adnceiba/entervehicle/view/EnterVehicleActivity.kt
+++ presentation/src/main/java/com/ceiba/adnceiba/entervehicle/view/EnterVehicleActivity.kt
@@ -5,6 +5,10 @@ import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.ceiba.adnceiba.R
 import com.google.android.material.textfield.TextInputEditText
 import java.text.SimpleDateFormat
@@ -12,6 +16,8 @@ import java.util.*
 
 class EnterVehicleActivity : AppCompatActivity() {
     private val mCalendar = Calendar.getInstance()
+
+    private val vehicles = arrayOf(CAR, MOTORCYCLE)
 
     private val mPickerDate = DatePickerDialog.OnDateSetListener { _, year, monthYear, dayMonth ->
         mCalendar.set(Calendar.YEAR, year)
@@ -34,10 +40,24 @@ class EnterVehicleActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_enter_vehicle)
 
+        initializeWidgets()
+    }
+
+    private fun initializeWidgets() {
         findViewById<TextInputEditText>(R.id.input_date_enter_vehicle).setOnClickListener {
             DatePickerDialog(this, mPickerDate, mCalendar.get(Calendar.YEAR), mCalendar.get(Calendar.MONTH),
                 mCalendar.get(Calendar.DAY_OF_MONTH)).show()
         }
+
+        val adapterSpinnerVehicle = ArrayAdapter(
+            this, android.R.layout.simple_spinner_item,
+            vehicles
+        )
+
+        val spinner = findViewById<Spinner>(R.id.spinner_vehicle_type_enter)
+        spinner.adapter = adapterSpinnerVehicle
+
+        getVehicleSelected(spinner)
     }
 
     private fun displayTimeDialog() {
@@ -51,5 +71,23 @@ class EnterVehicleActivity : AppCompatActivity() {
         val sdf = SimpleDateFormat(myFormat, Locale.ENGLISH)
 
         return sdf.format(mCalendar.time.time)
+    }
+
+    fun getVehicleSelected(spinner: Spinner) {
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                Log.e("VEHICULO", vehicles[position])
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+        }
+    }
+
+    companion object {
+        const val CAR = "Carro"
+        const val MOTORCYCLE = "Moto"
     }
 }
