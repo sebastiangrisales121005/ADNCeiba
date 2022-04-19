32bdc1dbad4ca843734e8adb9b9b18d13e05acb7
Juan Sebastian Grisales Muñoz <sebastian.grisales@ceiba.com.co>
2022-04-19 13:50:51 -0500

    Se elimina clase de ejemplo de prueba unitaria y se agrega método que calcula la cantidad de horas y días de duración del vehículo en el parqueadero

--- domain/src/main/java/com/ceiba/domain/valueobject/Time.kt
+++ domain/src/main/java/com/ceiba/domain/valueobject/Time.kt
@@ -1,11 +1,13 @@
 package com.ceiba.domain.valueobject
 
+import java.text.SimpleDateFormat
+
 class Time(startDateTime: String, endDateTime: String, day: String) {
     var startDateTime: String? = null
     var endDateTime: String? = null
     var day: String? = null
-    var numberDays: Int? = 0
-    var numberHours: Int? = null
+    var numberDays: Long? = 0
+    var numberHours: Long? = null
 
     init {
         this.startDateTime = startDateTime
@@ -17,6 +19,17 @@ class Time(startDateTime: String, endDateTime: String, day: String) {
         if (this.numberHours!! >= 9) {
             this.numberDays = this.numberDays!! + 1
         }
+    }
+
+    fun calculateTimeParking() {
+        val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+        val startDate = simpleDateFormat.parse(startDateTime!!)
+        val endDate = simpleDateFormat.parse(endDateTime!!)
+
+        val timeLong = endDate!!.time - startDate!!.time
+
+        this.numberDays = timeLong/(24 * 60 * 60 * 1000)
+        this.numberHours = (timeLong/(60 * 60 * 1000) - this.numberDays!! * 24)
     }
 
 }
