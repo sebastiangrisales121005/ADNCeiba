f7dc1e1bdda070b4fd8d3bd4a20267b8f1c8194c
Juan Sebastian Grisales Muñoz <sebastian.grisales@ceiba.com.co>
2022-04-19 09:53:42 -0500

    Se agrega prueba unitaria que valida al entrar en una excepción que si se arroje el mensaje esperado cuando el día no cumpla con la condición

--- domain/src/test/java/com/ceiba/domain/ParkingTest.kt
+++ domain/src/test/java/com/ceiba/domain/ParkingTest.kt
@@ -2,6 +2,7 @@ package com.ceiba.domain
 
 import com.ceiba.domain.aggregate.Parking
 import com.ceiba.domain.entity.Vehicle
+import com.ceiba.domain.exception.ParkingException
 import com.ceiba.domain.valueobject.Payment
 import com.ceiba.domain.valueobject.Time
 import org.junit.Assert
@@ -55,5 +56,37 @@ class ParkingTest {
 
         //Arrange
         Assert.assertFalse(result)
+    }
+
+    @Test
+    fun parking_validateEnterLicensePlateWithoutDay_exception() {
+        //Arrange
+        val licensePlate = "ABC000"
+        val typeVehicle = "Moto"
+        val cylinderCapacity = 150
+        val vehicle = Vehicle(licensePlate, typeVehicle, cylinderCapacity)
+
+        val startDateTime = "2022-04-14 08:00:00"
+        val endDateTime = "2022-04-14 10:00:00"
+        val day = "martes"
+        val time = Time(startDateTime, endDateTime, day)
+
+        val payment = Payment()
+
+        val parking = Parking(vehicle, time, payment)
+
+        val expectedMessage = "Este vehículo no está autorizado a ingresar"
+
+        //Act
+        try {
+            parking.validateEnterLicensePlate()
+
+            Assert.fail()
+
+        } catch (e: ParkingException) {
+            //Assert
+            Assert.assertEquals(expectedMessage, e.message)
+        }
+
     }
 }
