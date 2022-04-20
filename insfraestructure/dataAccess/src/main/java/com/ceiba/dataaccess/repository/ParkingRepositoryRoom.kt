fa8801c377d9a04fed29e74c261db092f5c0b94f
Juan Sebastian Grisales Muñoz <sebastian.grisales@ceiba.com.co>
2022-04-20 07:45:46 -0500

    Se agrega implementación de Room y se llama método que permite insertar el vehículo en la DB

--- insfraestructure/dataAccess/src/main/java/com/ceiba/dataaccess/repository/ParkingRepositoryRoom.kt
+++ insfraestructure/dataAccess/src/main/java/com/ceiba/dataaccess/repository/ParkingRepositoryRoom.kt
@@ -1,16 +1,30 @@
 package com.ceiba.dataaccess.repository
 
 import android.content.Context
+import androidx.room.Room
 import com.ceiba.dataaccess.anticorruption.ParkingTranslator
 import com.ceiba.domain.aggregate.Parking
 import com.ceiba.domain.repository.ParkingRepository
 import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class ParkingRepositoryRoom @Inject constructor(@ApplicationContext context: Context): ParkingRepository {
     private val context: Context = context
 
+    private val parkingDbRoomImpl: ParkingDbRoomImpl = Room.databaseBuilder(this.context, ParkingDbRoomImpl::class.java, DB_NAME).build()
+
     override fun enterVehicle(parking: Parking) {
         val parkingDto = ParkingTranslator.fromDomainToDto(parking)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            parkingDbRoomImpl.parkingDao().insertVehicle(parkingDto)
+        }
+    }
+
+    companion object {
+        const val DB_NAME = "PARKING"
     }
 }
