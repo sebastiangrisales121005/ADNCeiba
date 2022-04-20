8aa89769c61c4398c8034d0a8f08b718d200a271
Juan Sebastian Grisales Muñoz <sebastian.grisales@ceiba.com.co>
2022-04-20 07:23:47 -0500

    Se realiza inyección del contexto en el repository de dataAccess

--- insfraestructure/dataAccess/src/main/java/com/ceiba/dataaccess/repository/ParkingRepositoryRoom.kt
+++ insfraestructure/dataAccess/src/main/java/com/ceiba/dataaccess/repository/ParkingRepositoryRoom.kt
@@ -4,9 +4,11 @@ import android.content.Context
 import com.ceiba.dataaccess.anticorruption.ParkingTranslator
 import com.ceiba.domain.aggregate.Parking
 import com.ceiba.domain.repository.ParkingRepository
+import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
-class ParkingRepositoryRoom @Inject constructor(context: Context): ParkingRepository {
+class ParkingRepositoryRoom @Inject constructor(@ApplicationContext context: Context): ParkingRepository {
+    private val context: Context = context
 
     override fun enterVehicle(parking: Parking) {
         val parkingDto = ParkingTranslator.fromDomainToDto(parking)
