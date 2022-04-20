22b2abc079b20e4706c2b1976a2e2c00fc942d30
Juan Sebastian Grisales Muñoz <sebastian.grisales@ceiba.com.co>
2022-04-20 11:13:23 -0500

    Se agregan implementaciones para hacer uso de pruebas instrumentadas y se crea prueba de integración en el módulo de dataAccess para validar la integración con Room

--- insfraestructure/dataAccess/src/androidTest/java/com/ceiba/dataaccess/repository/ParkingDbRoomImplTest.kt
+++ insfraestructure/dataAccess/src/androidTest/java/com/ceiba/dataaccess/repository/ParkingDbRoomImplTest.kt
@@ -1,6 +1,56 @@
 package com.ceiba.dataaccess.repository
 
-import org.junit.Assert.*
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.ceiba.dataaccess.anticorruption.ParkingTranslator
+import com.ceiba.domain.aggregate.Parking
+import com.ceiba.domain.entity.Vehicle
+import com.ceiba.domain.valueobject.Time
+import junit.framework.TestCase
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
+class ParkingDbRoomImplTest: TestCase() {
+    private lateinit var parkingDbRoomImpl: ParkingDbRoomImpl
+    private lateinit var parkingServiceRoom: ParkingServiceRoom
 
-class ParkingDbRoomImplTest
+    @Before
+    public override fun setUp() {
+
+        val context = ApplicationProvider.getApplicationContext<Context>()
+
+        parkingDbRoomImpl = Room.inMemoryDatabaseBuilder(context, ParkingDbRoomImpl::class.java).build()
+        parkingServiceRoom = parkingDbRoomImpl.parkingDao()
+    }
+
+    @After
+    fun closeDb() {
+        parkingDbRoomImpl.close()
+    }
+
+    @Test
+    fun vehicle_enter_isCorrect() = runBlocking {
+        val licensePlate = "ABC000"
+        val typeVehicle = "Moto"
+        val cylinderCapacity = 150
+        val vehicle = Vehicle(licensePlate, typeVehicle, cylinderCapacity)
+
+        val startDateTime = "2022-04-14 08:00:00"
+        val endDateTime = "2022-04-14 10:00:00"
+        val day = "martes"
+        val time = Time(startDateTime, endDateTime, day)
+
+        val parkingDto = ParkingTranslator.fromDomainToDto(Parking(vehicle, time))
+
+        val id = parkingServiceRoom.insertVehicle(parkingDto)
+
+        Assert.assertNotNull(id)
+    }
+}
