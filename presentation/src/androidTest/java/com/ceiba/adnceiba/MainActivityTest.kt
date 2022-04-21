76d337a7b9c0b85a137ea476c71c89e8de9753e7
Juan Sebastian Grisales Muñoz <sebastian.grisales@ceiba.com.co>
2022-04-21 09:37:38 -0500

    Se agregan dependencias necesarias para uso de expresso en el módulo de presentation y se agrega prueba UI que verifica que muestre los componentes correspondientes al ingresar un vehículo

---
+++ presentation/src/androidTest/java/com/ceiba/adnceiba/MainActivityTest.kt
@@ -0,0 +1,30 @@
+package com.ceiba.adnceiba
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+    @Rule @JvmField
+    public val activityRule = ActivityScenarioRule(
+        MainActivity::class.java
+    )
+
+    @Test
+    fun clickEnterVehicleButton_openEnterUI() {
+        onView(withId(R.id.button_enter_vehicle)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.button_enter_vehicle)).perform(click())
+
+        onView(withId(R.id.button_save_vehicle)).check(matches(isDisplayed()))
+    }
+}
