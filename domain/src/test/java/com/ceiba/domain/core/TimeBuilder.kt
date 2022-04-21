4fb1d33e6e8f6fc8431d97de095b7abc445c4639
Juan Sebastian Grisales Muñoz <sebastian.grisales@ceiba.com.co>
2022-04-21 14:33:02 -0500

    Se agrega clase de TimeBuilder para trabajar las pruebas unitarias que reciben objetos de Time

---
+++ domain/src/test/java/com/ceiba/domain/core/TimeBuilder.kt
@@ -0,0 +1,40 @@
+package com.ceiba.domain.core
+
+import com.ceiba.domain.valueobject.Time
+
+class TimeBuilder() {
+    var startDateTime: String? = null
+    var endDateTime: String? = null
+    var day: String? = null
+
+    init {
+        startDateTime = ""
+        endDateTime = ""
+        day = ""
+    }
+
+    fun withStartDateTime(startDateTime: String): TimeBuilder {
+        this.startDateTime = startDateTime
+        return this
+    }
+
+    fun withEndDateTime(endDateTime: String): TimeBuilder {
+        this.endDateTime = endDateTime
+        return this
+    }
+
+    fun withDay(endDateTime: String): TimeBuilder {
+        this.day = day
+        return this
+    }
+
+    fun build(): Time {
+        return Time(startDateTime, endDateTime, day)
+    }
+
+    companion object {
+        fun aTime(): TimeBuilder {
+            return TimeBuilder()
+        }
+    }
+}
