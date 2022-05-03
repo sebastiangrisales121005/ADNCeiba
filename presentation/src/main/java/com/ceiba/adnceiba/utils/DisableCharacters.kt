7894e5ae6dc09ab7c9db9b39ac4961a255f28ecf
Juan Sebastian Grisales Muñoz <sebastian.grisales@ceiba.com.co>
2022-05-03 15:46:59 -0500

    Se segmenta método de disableEmoji y se agregan pruebas unitarias por cada método que lo compone

--- presentation/src/main/java/com/ceiba/adnceiba/utils/DisableCharacters.kt
+++ presentation/src/main/java/com/ceiba/adnceiba/utils/DisableCharacters.kt
@@ -7,7 +7,6 @@ class DisableCharacters {
         fun disableEmoji(): InputFilter {
             val emojiFilter = InputFilter { source, start, end, _, _, _ ->
                 for (index in start until end) {
-
                     when (Character.getType(source[index])) {
                         '*'.code,
                         Character.OTHER_SYMBOL.toInt(),
@@ -15,30 +14,16 @@ class DisableCharacters {
                             return@InputFilter ""
                         }
                         Character.LOWERCASE_LETTER.toInt() -> {
-                            val index2 = index + 1
-                            if (index2 < end && Character.getType(source[index + 1]) == Character.NON_SPACING_MARK.toInt())
-                                return@InputFilter ""
+                            return@InputFilter validateCharacterLowerCase(index, source, end)
                         }
                         Character.DECIMAL_DIGIT_NUMBER.toInt() -> {
-                            val index2 = index + 1
-                            val index3 = index + 2
-                            if (index2 < end && index3 < end &&
-                                Character.getType(source[index2]) == Character.NON_SPACING_MARK.toInt() &&
-                                Character.getType(source[index3]) == Character.ENCLOSING_MARK.toInt()
-                            )
-                                return@InputFilter ""
+                            return@InputFilter validateDecimalNumber(index, source, end)
                         }
                         Character.OTHER_PUNCTUATION.toInt() -> {
-                            val index2 = index + 1
-
-                            if (index2 < end && Character.getType(source[index2]) == Character.NON_SPACING_MARK.toInt()) {
-                                return@InputFilter ""
-                            }
+                            return@InputFilter validateOtherPunctuation(index, source, end)
                         }
                         Character.MATH_SYMBOL.toInt() -> {
-                            val index2 = index + 1
-                            if (index2 < end && Character.getType(source[index2]) == Character.NON_SPACING_MARK.toInt())
-                                return@InputFilter ""
+                            return@InputFilter validateMathSymbol(index, source, end)
                         }
                     }
                 }
@@ -47,5 +32,43 @@ class DisableCharacters {
 
             return emojiFilter
         }
+
+        fun validateCharacterLowerCase(index: Int, source: CharSequence, end: Int): String? {
+            val index2 = index + 1
+            if (index2 < end && Character.getType(source[index + 1]) == Character.NON_SPACING_MARK.toInt()) {
+                return ""
+            }
+            return null
+        }
+
+        fun validateDecimalNumber(index: Int, source: CharSequence, end: Int): String? {
+            val index2 = index + 1
+            val index3 = index + 2
+            if (index2 < end && index3 < end &&
+                Character.getType(source[index2]) == Character.NON_SPACING_MARK.toInt() &&
+                Character.getType(source[index3]) == Character.ENCLOSING_MARK.toInt()) {
+                return ""
+            }
+            return null
+        }
+
+        fun validateOtherPunctuation(index: Int, source: CharSequence, end: Int): String? {
+            val index2 = index + 1
+
+            if (index2 < end && Character.getType(source[index2]) == Character.NON_SPACING_MARK.toInt()) {
+                return ""
+            }
+            return null
+        }
+
+        fun validateMathSymbol(index: Int, source: CharSequence, end: Int): String? {
+            val index2 = index + 1
+            if (index2 < end && Character.getType(source[index2]) == Character.NON_SPACING_MARK.toInt()) {
+                return ""
+            }
+
+            return null
+        }
+
     }
 }
--- presentation/src/test/java/com/ceiba/adnceiba/DisableCharactersTest.kt
+++ presentation/src/test/java/com/ceiba/adnceiba/DisableCharactersTest.kt
@@ -14,4 +14,56 @@ class DisableCharactersTest {
         //Assert
         Assert.assertNotNull(filter)
     }
+
+    @Test
+    fun character_validateCharacterLowerCase_isFailure() {
+        //Arrange
+        val character = 0
+
+        //Act
+        val filter = DisableCharacters.validateCharacterLowerCase(character, "", character)
+
+        //Assert
+        Assert.assertNull(filter)
+
+    }
+
+    @Test
+    fun character_validateDecimalNumber_isFailure() {
+        //Arrange
+        val character = 0
+
+        //Act
+        val filter = DisableCharacters.validateDecimalNumber(character, "", character)
+
+        //Assert
+        Assert.assertNull(filter)
+
+    }
+
+    @Test
+    fun character_validateOtherPunctuation_isFailure() {
+        //Arrange
+        val character = 0
+
+        //Act
+        val filter = DisableCharacters.validateOtherPunctuation(character, "", character)
+
+        //Assert
+        Assert.assertNull(filter)
+
+    }
+
+    @Test
+    fun character_validateMathSymbol_isFailure() {
+        //Arrange
+        val character = 0
+
+        //Act
+        val filter = DisableCharacters.validateMathSymbol(character, "", character)
+
+        //Assert
+        Assert.assertNull(filter)
+
+    }
 }
