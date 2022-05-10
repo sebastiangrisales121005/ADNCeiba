5d2e3e8b99b433882f5dfe424e4cf7cd0e32e4ba
Juan Sebastian Grisales Muñoz <sebastian.grisales@ceiba.com.co>
2022-05-10 08:57:24 -0500

    Se refactoriza factory para que sean directamente un objeto

--- application/src/main/java/com/ceiba/application/service/factory/VehicleFactory.kt
+++ application/src/main/java/com/ceiba/application/service/factory/VehicleFactory.kt
@@ -4,22 +4,21 @@ import com.ceiba.domain.entity.Car
 import com.ceiba.domain.entity.Motorcycle
 import com.ceiba.domain.entity.Vehicle
 
-class VehicleFactory {
-    companion object {
-        const val MOTORCYCLE_ES = "Moto"
-        const val MOTORCYCLE_EN = "Motorcycle"
-        const val CAR_ES = "Carro"
-        const val CAR_EN = "Car"
+object VehicleFactory {
+    const val MOTORCYCLE_ES = "Moto"
+    const val MOTORCYCLE_EN = "Motorcycle"
+    const val CAR_ES = "Carro"
+    const val CAR_EN = "Car"
 
-        fun build(licensePlate: String, vehicleType: String?, cylinderCapacity: Int): Vehicle? {
-            var vehicle: Vehicle? = null
-            if (vehicleType == MOTORCYCLE_ES || vehicleType == MOTORCYCLE_EN) {
-                vehicle = Motorcycle(licensePlate, cylinderCapacity)
-            } else if (vehicleType == CAR_ES || vehicleType == CAR_EN) {
-                vehicle = Car(licensePlate)
-            }
-
-            return vehicle
+    fun build(licensePlate : String , vehicleType : String? , cylinderCapacity : Int) : Vehicle? {
+        var vehicle : Vehicle? = null
+        if (vehicleType == MOTORCYCLE_ES || vehicleType == MOTORCYCLE_EN) {
+            vehicle = Motorcycle(licensePlate , cylinderCapacity)
+        } else if (vehicleType == CAR_ES || vehicleType == CAR_EN) {
+            vehicle = Car(licensePlate)
         }
+
+        return vehicle
     }
+
 }
--- insfraestructure/dataAccess/src/main/java/com/ceiba/dataaccess/factory/EnterVehicleFactory.kt
+++ insfraestructure/dataAccess/src/main/java/com/ceiba/dataaccess/factory/EnterVehicleFactory.kt
@@ -7,14 +7,14 @@ import com.ceiba.dataaccess.repository.CarEnterRepository
 import com.ceiba.dataaccess.repository.MotorcycleEnterRepository
 import com.ceiba.domain.repository.VehicleEnterRepository
 
-class EnterVehicleFactory {
-    companion object {
-        fun build(vehicleType: String?): VehicleEnterRepository {
-            var vehicle: VehicleEnterRepository = CarEnterRepository()
-            if (vehicleType == MOTORCYCLE_ES || vehicleType == MOTORCYCLE_EN) {
-                vehicle = MotorcycleEnterRepository()
-            }
-            return vehicle
+object EnterVehicleFactory {
+
+    fun build(vehicleType : String?) : VehicleEnterRepository {
+        var vehicle : VehicleEnterRepository = CarEnterRepository()
+        if (vehicleType == MOTORCYCLE_ES || vehicleType == MOTORCYCLE_EN) {
+            vehicle = MotorcycleEnterRepository()
         }
+        return vehicle
     }
+
 }
